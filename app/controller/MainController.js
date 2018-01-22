f17ddd1527e4a0c007944675126d4e1ade5f7ab8
Chetan <chetan.ac.in@gmail.com>
2018-01-21 19:17:31 -0500

    Code cleanup after creating the app

--- app/controller/MainController.js
+++ app/controller/MainController.js
@@ -1,35 +1,9 @@
+/**
+ * STouchApp.controller.MainController is the extension of Ext.app.Controller.
+ * This is to load tabs object coming from the server or a json file.
+ */
 Ext.define('STouchApp.controller.MainController', {
     extend: 'Ext.app.Controller',
-    models: ['Tab'],
-    stores: ['Tabs'],
-
-    config: {
-        refs: {
-            tabListStore: 'STouchApp.store.TabListStore'
-        }
-        /*refs: {
-            loginForm: 'formpanel'
-        },
-        control: {
-            'formpanel button': {
-                tap: 'doLogin'
-            }
-        }*/
-    },
-
-    init: function() {
-        //alert('MainController');
-        //this.loadTabStore(); // Call this function to load the Tabs store from server side data.
-    }//,
-
-    /*loadTabStore: function() {
-        let me = this;
-        debugger;
-        me.getTabsStore().load({
-            params : {},
-            success : function() {},
-            failure : function() {},
-            callback : function() {}
-        });
-    }*/
+    models: ['Tab'],// This entry will load the Tab model's JS file beforehand.
+    stores: ['Tabs']// This entry will load the Tab store's JS file beforehand.
 });
